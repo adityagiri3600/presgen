34be8549e78b34e5362312572837a3eb8758739f
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-04-29 19:27:20 +0530

    power notation

--- presgen.py
+++ presgen.py
@@ -1,16 +1,17 @@
 class Group:
     
-    def __init__(self,presentation_string):
-        
+    def __init__(self,presentation_string, identity_symbol = "e", abelian=False):
+        self.identity_symbol = identity_symbol
         presentation_string_split = presentation_string[1:-1].split("|")
         self.alphabet = presentation_string_split[0].split(",")
         self.constraints = presentation_string_split[1].split(",")
-        self.words = self.generate_reduced_words(self.alphabet, self.constraints)
+        self.words = self.generate_reduced_words(self.alphabet, self.constraints, abelian=abelian)
+        self.order = len(self.words)
         self.table = self.multiplication_table()
         
-    def generate_reduced_words(self,alphabet, constraints,word_length_limit=10):
+    def generate_reduced_words(self,alphabet, constraints, abelian = False, word_length_limit = 10):
         reduced_words = set()
-        constraints = self.clean_constraints(alphabet,constraints)
+        constraints = self.clean_constraints(alphabet,constraints,abelian=abelian)
 
         def dfs(word):
             if len(word)>word_length_limit:
@@ -25,41 +26,52 @@ class Group:
         
         dfs("")
         reduced_words = list(reduced_words)
-        reduced_words.sort()
-        reduced_words[0] = 'e'
+        reduced_words.sort(key= lambda x: (len(x), x))
+        reduced_words[0] = self.identity_symbol
         return reduced_words
 
-    def clean_constraints(self, alphabet,constraints):
+    def clean_constraints(self, alphabet,constraints,abelian=False):
         
+        e = self.identity_symbol
+        
+        for i in range(len(constraints)):
+            constraints[i] = self.expand_powers(constraints[i])
+        
+        if abelian:
+            for i in range(len(alphabet)):
+                for j in range(i+1,len(alphabet)):
+                    constraints.append(alphabet[j] + alphabet[i] + "=" + alphabet[i] + alphabet[j])
+                    
         for i in range(len(constraints)):
             a,b = constraints[i].split("=")
             while a[0]==b[0]:
                 a = a[1:]
                 b = b[1:]
                 if b=="":
-                    b = "e"
+                    b = e
                     break
                 if a=="":
-                    a = "e"
+                    a = e
                     break
             while a[-1]==b[-1]:
                 a = a[:-1]
                 b = b[:-1]
                 if b=="":
-                    b = "e"
+                    b = e
                     break
                 if a=="":
-                    a = "e"
+                    a = e
                     break
-            if a=="e" or (not b=="e" and a<b):
+            if a==e or (not b==e and a<b):
                 constraints[i] = b + "=" + a
             else:
                 constraints[i] = a + "=" + b
         
-        constraints.append("ee=e")
+        constraints.append(e + e + "=" + e)
         for a in alphabet:
-            constraints.append(f'{a}e={a}')
-            constraints.append(f'e{a}={a}')
+            constraints.append(f'{a}{e}={a}')
+            constraints.append(f'{e}{a}={a}')
+            
         return constraints
         
     def satisfies_constraints(self, word, constraints):
@@ -78,26 +90,48 @@ class Group:
                 return True
         return False
 
-
-    def multiply(self, a,b,constraints):
-        word = a+b
-        def replace_till_no_change(word):
-            original_word = word
-            for constraint in constraints:
-                a,b = constraint.split("=")
-                word = word.replace(a,b)
-            if word == original_word:
-                return word
+    def expand_powers(self, word):
+        expanded_word = ""
+        i = 0
+        while i < len(word):
+            if i+2 < len(word) and word[i+2].isdigit():
+                expanded_word += word[i]*int(word[i+2])
+                i += 3
             else:
-                return replace_till_no_change(word)
-        return replace_till_no_change(word)
+                expanded_word += word[i]
+                i += 1
+        return expanded_word
+    def collect_powers(self, word):
+        collected_word = ""
+        i = 0
+        while i < len(word):
+            count = 1
+            while i+1 < len(word) and word[i] == word[i+1]:
+                count += 1
+                i += 1
+            if count == 1:
+                collected_word += word[i]
+            else:
+                collected_word += word[i] + "^" + str(count)
+            i += 2
+        return collected_word
+
+    def simplify(self, word, constraints):
+        original_word = word
+        for constraint in constraints:
+            a,b = constraint.split("=")
+            word = word.replace(a,b)
+        if word == original_word:
+            return self.collect_powers(word)
+        else:
+            return self.simplify(word, constraints)
 
     def multiplication_table(self):
         table = []
         for a in self.words:
             row = []
             for b in self.words:
-                row.append(self.multiply(a,b,self.constraints))
+                row.append(self.simplify(a+b,self.constraints))
             table.append(row)
         return table
 
@@ -109,16 +143,42 @@ class Group:
                     padding = len(word)+2
         
         
-        print(''.ljust(padding), end='')
+        print(''.ljust(padding), end='|'.ljust(padding))
         for word in self.table[0]:
             print(word.ljust(padding), end='')
         print()
+        for word in self.table[0]:
+            print("-"*(len(word.ljust(padding))+2), end='')
+        print()
         for row in self.table:
-            print(row[0].ljust(padding), end='')
+            print(row[0].ljust(padding), end='|'.ljust(padding))
             for word in row:
                 print(word.ljust(padding), end='')
             print()
+            
+    def __str__(self):
+        string = ""
+        padding = 4
+        for row in self.table:
+            for word in row:
+                if len(word)+2 > padding:
+                    padding = len(word)+2
+        string += ''.ljust(padding) + '|'.ljust(padding)
+        for word in self.table[0]:
+            string += word.ljust(padding)
+        string += '\n'
+        for word in self.table[0]:
+            string += "-"*(len(word.ljust(padding))+2)
+        string += '\n'
+        for row in self.table:
+            string += row[0].ljust(padding) + '|'.ljust(padding)
+            for word in row:
+                string += word.ljust(padding)
+            string += '\n'
+        return string
+        
 
 
-klein_group = Group("<a,b|aa=e,bb=e,ab=ba>")
-klein_group.show_table()
+fourth_roots_of_unity = Group("<i|i^4=1>",abelian=True,identity_symbol="1")
+print("Order:",fourth_roots_of_unity.order)
+print(fourth_roots_of_unity)
